271b2377b287d6aa6cf6d112e65a258b2538aa3f
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-27 18:45:23 -0300

    migrate

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use App\Http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 
-Route::view('/Produtos', 'products');
-
-Route::view('/Contatos', 'contact');
 
 // Route::get('/eventos', function ($id = 'indefinido') {
 //     // eventos?categoria=id
