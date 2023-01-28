2c2b50333ebaf18297393c70985dc4726bde923a
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-27 22:33:28 -0300

    home completa

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::view('/contact', 'contact');
 
 
 // Route::get('/eventos', function ($id = 'indefinido') {
