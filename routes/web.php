fbdc371ab33af86d14534b009e3fc77c2a67a0a8
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-31 10:56:27 -0300

    show

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 Route::view('/contact', 'contact');
 Route::post('/events', [EventController::class, 'store']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 
 
 // Route::get('/eventos', function ($id = 'indefinido') {
